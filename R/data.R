b1d1dcb4bf0c133278cf28526785ca0f429bc062
alex <c.alex.pellett@gmail.com>
2018-12-26 20:41:37 -0500

    start documenting the data

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' @title blockGroups
+#' @description
+#'
+#' @format An sf polygon object with 18 variables
+#' \describe{
+#'   \item{bgNumber}{}
+#'   \item{areaLand}{}
+#'   \item{areaWater}{}
+#'   \item{year}{}
+#'
+#'   }
+#' @source American Community Survey
+"blockGroups"
+
+#' @title places
+#' @description
+#'
+#' @format An sf polygon object with 29 variables
+#' \describe{
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   }
+#'
+#' @source American Community Survey
+"places"
+
+
+#' @title tracts
+#' @description
+#'
+#' @format An sf polygon object with 31 variables
+#' \describe{
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   }
+#'
+#' @source American Community Survey
+"tracts"
